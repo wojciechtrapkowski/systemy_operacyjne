489dd37c4bff47f66b041f060a8551b9ec52d32a
Wojtek <Wojtek@LAPTOP-JT684LJQ>
2023-03-24 12:20:27 +0100

    finished, need to clean code

--- zad_2.sh
+++ zad_2.sh
@@ -3,22 +3,29 @@ option=0
 nazwa=""
 katalog=""
 zawartosc=""
+rozmiar=""
+empty=0
+delete=0
 
 nameOption=""
 dirOption=""
 zawOption=""
+rozmiarOption=""
+emptyOption=""
+deleteOption=""
 
 findMethod=""
 c='"'
+
 echo $c
 while [[ $option -ne 8 ]] 
 	do
 		echo "Menu"
 		echo "1. Nazwa ${nazwa}"
 		echo "2. Katalog ${katalog}" 
-		echo "3. "
-		echo "4. "
-		echo "5. "
+		echo "3. Rozmiar pliku ${rozmiar}"
+		echo "4. Czy plik ma byc pusty [0/1] ${empty}"
+		echo "5. Czy chcesz usunac pliki [0/1] ${delete}"
 		echo "6. Zawartosc ${zawartosc}"
 		echo "7. Szukaj" 
 		echo "8. Wyjscie"
@@ -43,6 +50,33 @@ while [[ $option -ne 8 ]]
 				dirOption="";
 			fi
 ;;			
+			"3") echo "Podaj rozmiar pliku";
+			read rozmiar;
+			if [ "$rozmiar" != "" ]
+			then
+				rozmiarOption="-size ${rozmiar}";
+			else
+				rozmiarOption="";
+			fi
+;;
+			"4") echo "Czy plik ma byc pusty [0/1]";
+				read empty;
+				if [ "$empty" = "1" ]
+				then
+					emptyOption="-empty";
+				else
+					emptyOption="";
+				fi
+			;;
+			"5") echo "Czy plik ma byc usuniety [0/1]";
+			read delete;
+			if [ "$delete" != "" ]
+			then
+				deleteOption="-delete";
+			else
+				deleteOption="";
+			fi
+;;
 			"6") echo "Podaj zawartosc";
 			read zawartosc;
 			if [ "$zawartosc" != "" ]
@@ -53,7 +87,7 @@ while [[ $option -ne 8 ]]
 			fi
 ;;
 			"7")  
-				findMethod="find ${dirOption} ${nameOption} -type f ${zawOption}";
+				findMethod="find ${dirOption} ${nameOption} ${rozmiarOption} ${emptyOption} ${deleteOption} -type f ${zawOption}";
 				eval $findMethod;	
 ;;
 			"8") option=8;;
